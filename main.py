ed6708d894f209483e25a626e57f9d28f6b61eda
CommuLord <commulord20@mail.ru>
2024-06-26 20:59:00 +0300

    bd

--- main.py
+++ main.py
@@ -19,8 +19,67 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), index=True)  # Указываем длину для VARCHAR
-    email = Column(String(100), unique=True, index=True)  # Указываем длину для VARCHAR
+    name = Column(String(50), index=True)
+    email = Column(String(100), unique=True, index=True)
+
+class Dish(Base):
+    __tablename__ = "dishes"
+
+    dish_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    type = Column(String(50))
+    portion_size = Column(Integer)
+    image_url = Column(String(255))
+    recipes_count = Column(Integer, default=0)
+    preparations_count = Column(Integer, default=0)
+    ingredients_count = Column(Integer, default=0)
+
+class Product(Base):
+    __tablename__ = "products"
+
+    product_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    calories_per_100g = Column(Float)
+    weight = Column(Integer)
+    price_per_kg = Column(Float)
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    recipe_id = Column(Integer, primary_key=True, index=True)
+    dish_id = Column(Integer, ForeignKey('dishes.dish_id'))
+    preparation_time_minutes = Column(Integer)
+    cooking_technique = Column(String(255))
+
+    dish = relationship("Dish", back_populates="recipes")
+
+Dish.recipes = relationship("Recipe", order_by=Recipe.recipe_id, back_populates="dish")
+
+class Preparation(Base):
+    __tablename__ = "preparations"
+
+    preparation_id = Column(Integer, primary_key=True, index=True)
+    dish_id = Column(Integer, ForeignKey('dishes.dish_id'))
+    portion_count = Column(Integer)
+    date = Column(DateTime)
+
+    dish = relationship("Dish", back_populates="preparations")
+
+Dish.preparations = relationship("Preparation", order_by=Preparation.preparation_id, back_populates="dish")
+
+class DishIngredient(Base):
+    __tablename__ = "dish_ingredients"
+
+    dish_id = Column(Integer, ForeignKey('dishes.dish_id'), primary_key=True)
+    product_id = Column(Integer, ForeignKey('products.product_id'), primary_key=True)
+    quantity = Column(Integer)
+
+    dish = relationship("Dish", back_populates="ingredients")
+    product = relationship("Product", back_populates="in_dishes")
+
+Dish.ingredients = relationship("DishIngredient", back_populates="dish")
+Product.in_dishes = relationship("DishIngredient", back_populates="product")
+
 
 # Создание таблиц в базе данных
 Base.metadata.create_all(bind=engine)
